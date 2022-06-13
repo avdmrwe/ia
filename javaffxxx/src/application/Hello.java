f59040d95eebbe5c641f8e18b142b1513614e9ad
avdmrwe <93856867+avdmrwe@users.noreply.github.com>
2022-06-13 21:23:12 +0400

    Update Hello.java

--- javaffxxx/src/application/Hello.java
+++ javaffxxx/src/application/Hello.java
@@ -3,9 +3,11 @@ package application;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 
@@ -20,6 +22,17 @@ public class Hello extends Application {
 				Scene scene = new Scene(root);
 				primaryStage.setScene(scene);
 				primaryStage.show();
+				
+				
+				
+			    root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			        @Override
+			        public void handle(KeyEvent event) {
+			        	System.out.println("key pressed: " + event.getCode().toString());
+			        	
+			        }
+			    });
+
 
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -28,6 +41,11 @@ public class Hello extends Application {
 			
 	}
 	
+	
+	
+        	
+        	
+        	
 	public static void main(String[] args) {
 		launch(args);
 	}
