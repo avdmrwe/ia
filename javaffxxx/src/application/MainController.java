797aa2039342ed0c783ed2d71e788c40f9dd6bc3
avdmrwe <93856867+avdmrwe@users.noreply.github.com>
2022-06-13 20:55:29 +0400

    Add files via upload

---
+++ javaffxxx/src/application/MainController.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.net.URL;
+
+import java.util.ResourceBundle;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.util.Duration;
+
+public class MainController implements Initializable{
+	
+	@FXML
+	private Button clickme;
+	
+	private double deltaX = 5;
+	
+	
+	Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
+		
+		@FXML
+		public void handle(ActionEvent actionEvent) {
+			clickme.setLayoutX(clickme.getLayoutX() + deltaX);
+
+		}
+	
+	}));
+
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		timeline.setCycleCount(Animation.INDEFINITE);
+		timeline.play();
+	}
+}
+
---
+++ javaffxxx/src/module-info.java
@@ -0,0 +1,6 @@
+module javaffxxx {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
