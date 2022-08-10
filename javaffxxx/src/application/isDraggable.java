29a8c91b4e27e2bc176b0b3ed8ed882b40c04dba
avdmrwe <93856867+avdmrwe@users.noreply.github.com>
2022-08-10 16:02:20 +0400

    Create isDraggable.java

---
+++ javaffxxx/src/application/isDraggable.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+
+public class isDraggable {
+	
+	private static double mousePosX;
+	private static double mousePosY;
+	
+	public static void dragThis(Node node) {
+		
+		node.setOnMousePressed(mouseEvent -> {
+			mousePosX = mouseEvent.getX();
+			mousePosY = mouseEvent.getY();
+		});
+		
+		node.setOnMouseDragged(mouseEvent -> {
+			node.setLayoutX(mouseEvent.getSceneX() - mousePosX);
+			node.setLayoutY(mouseEvent.getSceneY() - mousePosY);
+			
+
+		});
+	}
+	
+	
+}
